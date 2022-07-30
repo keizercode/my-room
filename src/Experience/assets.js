a74982ff3af2bfbfb4a4a5327080fb430e3b1a17
keizercode <luqman.arohim@if.uai.ac.id>
2022-07-30 19:59:27 +0700

    initial commit

---
+++ src/Experience/assets.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'reactlogo', source: '/assets/reactLogo-1674x526.png', type: 'texture' },
+            
+            { name: 'pcScreenModel', source: '/assets/pcScreenModel.glb', type: 'model' },
+            { name: 'roomModel', source: '/assets/roomModel.glb' },
+        ]
+    }
+]
